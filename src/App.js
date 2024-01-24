b6fac126697ff591cc1acdd1f1b77e0ca69e47da
Jimin <122578483+jiminpark23@users.noreply.github.com>
2024-01-24 15:38:53 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+import TodoList from './components/TodoList.js';
+
+function App() {
+
+  return (
+    <div className="App">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+
+export default App;
